bca0f9a2bc122bbc6860bb688d8f9673655fdc34
Josh-Wickens <joshwickens@hotmail.com>
2023-01-04 19:56:44 +0000

    amend validaters

--- posts/serializers.py
+++ posts/serializers.py
@@ -6,21 +6,19 @@ from profiles.models import Profile
 class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
-    profile_id = serializers.ReadOnlyField(source="owner.profile.id")
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
 
     def validate_image(self, value):
-        if value.size > 1024 * 1024 * 2:
+        if value.size > 2 * 1024 * 1024:
+            raise serializers.ValidationError('Image size larger than 2MB!')
+        if value.image.height > 4096:
             raise serializers.ValidationError(
-                'Image size larger than 2MB!'
+                'Image height larger than 4096px!'
             )
         if value.image.width > 4096:
             raise serializers.ValidationError(
-                'Image width larger than 4096px'
-            )
-        if value.image.height > 4096:
-            raise serializers.ValidationError(
-                'Image height larger than 4096px'
+                'Image width larger than 4096px!'
             )
         return value
 
@@ -31,29 +29,7 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = [
-            'id', 'owner', 'created_at', 'updated_at', 'name',
-            'content', 'image', 'is_owner', 'profile_id', 'profile_image', 'image_filter'
+            'id', 'owner', 'is_owner', 'profile_id',
+            'profile_image', 'created_at', 'updated_at',
+            'title', 'content', 'image', 'image_filter'
         ]
-
-
-
-
-
-
-
-
-
-# class ProfileSerializer(serializers.ModelSerializer):
-#     owner = serializers.ReadOnlyField(source='owner.username')
-#     is_owner = serializers.SerializerMethodField()
-
-#     def get_is_owner(self, obj):
-#         request = self.context['request']
-#         return request.user == obj.owner
-
-#     class Meta:
-#         model = Profile
-#         fields = [
-#             'id', 'owner', 'created_at', 'updated_at', 'name',
-#             'content', 'image', 'is_owner'
-#         ]
